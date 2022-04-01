53111bb34a0a7c7016cb5cc76193d8d5c5af4ed3
Abbos2101 <abbos2101@gmail.com>
2022-04-02 03:24:19 +0500

    - lock landscape mode

--- lib/di/di.dart
+++ lib/di/di.dart
@@ -1,3 +1,4 @@
+import 'package:flutter/services.dart';
 import 'package:get_it/get_it.dart';
 import 'package:weather_task1_app/data/api/api_base.dart';
 import 'package:weather_task1_app/data/api/task_api.dart';
@@ -7,6 +8,10 @@ import 'package:weather_task1_app/data/hive/task_hive.dart';
 final GetIt di = GetIt.instance;
 
 Future<void> setup() async {
+  await SystemChrome.setPreferredOrientations([
+    DeviceOrientation.portraitUp,
+    DeviceOrientation.portraitDown,
+  ]);
   di.registerSingleton(ApiBase());
   di.registerFactory(() => TaskApi(di.get<ApiBase>().dio));
   di.registerSingleton(HiveBase());
